56691de7e98392e04a54bfe261b88ac474b839d0
Tsahi Edri <TsahiEdri1@gmail.com>
2022-08-21 10:56:31 +0300

    Control flow fixed, added "try again" option

--- src/Runner.java
+++ src/Runner.java
@@ -3,21 +3,64 @@ import javax.swing.*;
 public class Runner {
     public static void main(String[] args) {
         Function f = new Function();
-
+        Object[] option1 = {"ILS", "GBP", "Quit"};
+        Object[] option2 = {"Try Again", "Quit"};
 
 
         while(true) {
-            String input = JOptionPane.showInputDialog(null, "Enter value: ");
+            String input = JOptionPane.showInputDialog(null, "Enter amount of dollars to convert: ");
             System.out.println(input);
 
             if(f.inputIsValid(input)){
                 double amountToConvert = Double.parseDouble(input);
-                f.dollarToILS(amountToConvert);
-                f.dollarToGBP(amountToConvert);
+
+                int choice1 = JOptionPane.showOptionDialog(null,
+                        "Choose currency to convert to: ",
+                        "Currency Converter",
+                        JOptionPane.YES_NO_CANCEL_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        null,
+                        option1,
+                        option1[2]);
+                switch (choice1){
+                    case 0 -> {
+                        f.dollarToILS(amountToConvert);
+                        int choice2 = JOptionPane.showOptionDialog(null,
+                                "What do you wish to do?"
+                                , "Currency Converter",
+                                JOptionPane.YES_NO_CANCEL_OPTION,
+                                JOptionPane.QUESTION_MESSAGE,
+                                null, option2, option2[1]);
+                        if(choice2 == 0){
+                            continue;
+                        }
+                    }
+                    case 1 -> {
+                        f.dollarToGBP(amountToConvert);
+                        int choice2 = JOptionPane.showOptionDialog(null,
+                                "What do you wish to do?"
+                                , "Currency Converter",
+                                JOptionPane.YES_NO_CANCEL_OPTION,
+                                JOptionPane.QUESTION_MESSAGE,
+                                null, option2, option2[1]);
+                        if(choice2 == 0){
+                            continue;
+                        }
+                    }
+                }
                 break;
             }
             else{
-                JOptionPane.showMessageDialog(null, "Invalid input, please try again.");
+                JOptionPane.showMessageDialog(null, "Invalid input");
+                int choice2 = JOptionPane.showOptionDialog(null,
+                        "What do you wish to do?"
+                            , "Currency Converter",
+                        JOptionPane.YES_NO_CANCEL_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        null, option2, option2[1]);
+                if(choice2 == 0){
+                    continue;   
+                } else{break; }
             }
         }
 
